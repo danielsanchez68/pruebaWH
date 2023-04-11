84b47d799627e914708023701a07700e61de88aa
danielsanchez68 <danielsanchez68@hotmail.com>
2023-04-11 08:56:05 -0300

    v2

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
 const express = require('express')
-
+const crypto = require('crypto');
+const { execSync } = require('child_process');
 
 const app = express()
 
+
+app.post('/git', (req, res) => {
+  const hmac = crypto.createHmac('sha1', process.env.SECRET);
+  const sig  = 'sha1=' + hmac.update(JSON.stringify(req.body)).digest('hex');
+  if (req.headers['x-github-event'] === 'push' &&
+    crypto.timingSafeEqual(Buffer.from(sig), Buffer.from(req.headers['x-hub-signature']))) {
+    res.sendStatus(200);
+    const commands = ['git fetch origin master',
+                      'git reset --hard origin/master',
+                      'git pull origin master --force',
+                      'npm install',
+                      // your build commands here
+                      'refresh']; // fixes glitch ui
+    for (const cmd of commands) {
+      console.log(execSync(cmd).toString());
+    }
+    console.log('updated with origin/master!');
+    return;
+  } else {
+    console.log('webhook signature incorrect!');
+    return res.sendStatus(403);
+  }
+});
+
 app.get('/', (req,res) => {
-    res.send('Hola mundo 1')
+    res.send('Hola mundo 2')
 })
 
 const PORT = process.env.PORT || 8080
